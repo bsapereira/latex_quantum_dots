365b86137df69d9498b03fb46a22d8802ce690b0
Bruno Sardinha <brunosardinha111@gmail.com>
2017-06-06 18:36:28 -0300

    Distribuição em arquivos, correções, referência de 2.2, 2.3

--- check_modify.sh
+++ check_modify.sh
@@ -1,4 +1,4 @@
 while true; do
-    inotifywatch -e modify -t 1 Modelo_Monografia.tex 2>/dev/null | grep total &&
+    inotifywatch -e modify -t 1 *.tex 2>/dev/null | grep total &&
    pdflatex Modelo_Monografia.tex
 done
